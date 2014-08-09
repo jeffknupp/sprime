1be95061ae90e2be06ea9b667b870f07218e89d1
Jeff Knupp <jeff@jeffknupp.com>
2014-08-09 14:54:33 -0400

    Fix path for db

--- examples/runserver.py
+++ examples/runserver.py
@@ -1,6 +1,6 @@
 from sandman import reflect_all_app
 
-app = reflect_all_app('sqlite+pysqlite:///db.sqlite3')
+app = reflect_all_app('sqlite+pysqlite:///examples/db.sqlite3')
 
 def main():
     app.run(debug=True)
