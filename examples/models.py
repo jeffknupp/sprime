7dd6d2bc6a512a13bc13feb06fa971cd5809d91e
Jeff Knupp <jeff@jeffknupp.com>
2014-08-11 11:26:46 -0400

    add models to example file

---
+++ examples/models.py
@@ -0,0 +1,59 @@
+from sandman import register, Model, db
+
+
+class SomeModel(db.Model):
+    __tablename__ = 'some_model'
+
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.String)
+
+
+class Track(Model):
+    __tablename__ = 'Track'
+
+    def __str__(self):
+        return self.Name
+
+
+class Artist(Model):
+    __tablename__ = 'Artist'
+
+    def __str__(self):
+        return self.Name
+
+
+class Album(Model):
+    __tablename__ = 'Album'
+
+    def __str__(self):
+        return self.Title
+
+
+class Playlist(Model):
+    __tablename__ = 'Playlist'
+
+    def __str__(self):
+        return self.Name
+
+
+class PlaylistTrack(Model):
+    __tablename__ = 'PlaylistTrack'
+
+    def __str__(self):
+        return self.track.TrackId
+
+
+class MediaType(Model):
+    __tablename__ = 'MediaType'
+
+    def __str__(self):
+        return self.Name
+
+
+class Genre(Model):
+    __tablename__ = 'Genre'
+
+    def __str__(self):
+        return self.Name
+
+register([Artist, Track, Album, Playlist, PlaylistTrack, MediaType, Genre, SomeModel])
